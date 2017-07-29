126f7f7ca2ef55034598ca307c1f15e3fd144803
Soumya Shukla <sshukla@walmartlabs.com>
2017-07-29 09:45:15 +0530

    Removing older code

--- src/components/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import LandingPage from './LandingPage'
-
-const App = ({children, ...props}) => (
-  <div>
-    {children || <LandingPage {...props} />}
-  </div>
-)
-export default App
